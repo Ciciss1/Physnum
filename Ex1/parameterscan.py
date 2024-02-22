91d48c89669f2d0d615c59cc0d174ad93741419b
Ciciss1 <160743697+Ciciss1@users.noreply.github.com>
2024-02-22 01:24:13 +0000

    hop

--- Ex1/parameterscan.py
+++ Ex1/parameterscan.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Parameters
 # TODO adapt to what you need (folder path executable input filename)
 repertoire = './'  # Path to the compiled code (NB: ./ is not required on Windows)
-executable = 'exe'  # Name of the executable (NB: .exe extension is required on Windows)
+executable = 'Exercice1_student.exe'  # Name of the executable (NB: .exe extension is required on Windows)
 input_filename = 'configuration.in.example'  # Name of the input file
 
 
@@ -18,7 +18,7 @@ dt = tfin / nsteps
 
 # Analysis
 # TODO insert the values
-m = 0  
+m = 0.056  
 v = 0
 omega = 0  
 # add the other variables
