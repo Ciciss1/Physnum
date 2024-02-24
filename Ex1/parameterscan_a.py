ba8bafc9c9163ddd1c3690287420e344df1c99b5
ferocitoo <116436851+ferocitoo@users.noreply.github.com>
2024-02-24 21:36:40 +0000

    wsh la zone

--- Ex1/parameterscan_a.py
+++ Ex1/parameterscan_a.py
@@ -9,24 +9,33 @@ executable = 'Exercice1_student'  # Name of the executable (NB: .exe extension i
 input_filename = 'configuration.in.example'  # Name of the input file
 
 
-nsteps = np.array([400, 800]) # TODO change
+nsteps = np.arange(50,1000,50) # TODO change
 nsimul = len(nsteps)  # Number of simulations to perform
 
-tfin = 600  # TODO: Verify that the value of tfin is EXACTLY the same as in the input file
+tfin = 60 # TODO: Verify that the value of tfin is EXACTLY the same as in the input file
 
 dt = tfin / nsteps
+
+pi = np.pi
+g = 9.81  # m/s^2
 
 # Analysis
 # TODO insert the values
 m = 0.056  
-v = 0
-omega = 0  
+
+v0 = 5
+omega = 10
+mu = 6
+R = 0.033
+rho = 1.2
+
+gamma = mu * (R**3) * rho * omega/m
 # add the other variables
 # TODO: Insert here the expressions for the exact final solution
-x_th  = 0 
-y_th  = 0
-vx_th = 0
-vy_th = 0
+x_th  = (v0/gamma)*np.sin(gamma*tfin)
+y_th  = (v0/gamma)*(1- np.cos(gamma*tfin))
+vx_th = v0*np.cos(gamma*tfin)
+vy_th = v0*np.sin(gamma*tfin)
 """
 ... and other parameters
 """
@@ -47,6 +56,7 @@ for i in range(nsimul):
 
 
 error = np.zeros(nsimul)
+error_mec = np.zeros(nsimul)
 
 for i in range(nsimul):  # Iterate through the results of all simulations
     data = np.loadtxt(outputs[i])  # Load the output file of the i-th simulation
@@ -57,9 +67,14 @@ for i in range(nsimul):  # Iterate through the results of all simulations
     vx = data[-1, 3]
     vy = data[-1, 4]
     En = data[-1, 5]
+    
+    En_min = np.min(data[:, 5])
+    En_max = np.max(data[:, 5])
+    
     vy_list.append(vy)
     # TODO compute the error for each simulation
-    error[i] =  100 
+    error[i] = np.sqrt((x_th-xx)**2 + (y_th-yy)**2)
+    error_mec[i] = En_max-En_min
 
 lw = 1.5 # line width. TODO: adjust if needed
 fs = 16  # font size. TODO: adjust if needed
@@ -68,6 +83,8 @@ fig, ax = plt.subplots(constrained_layout=True)
 ax.plot(data[:, 1], data[:, 2])
 ax.set_xlabel('x [m]', fontsize=fs)
 ax.set_ylabel('y [m]', fontsize=fs)
+plt.tight_layout()
+plt.axis('equal')
 plt.savefig('plot1.png')
 
 
@@ -77,12 +94,23 @@ plt.savefig('plot1.png')
 #pbd.set_trace()
 plt.figure()
 plt.loglog(dt, error, 'r+-', linewidth=lw)
-plt.xlabel('\Delta t [s]', fontsize=fs)
+plt.xlabel(r'\Delta t [s]', fontsize=fs)
 plt.ylabel('final position error [m]', fontsize=fs)
 plt.xticks(fontsize=fs)
 plt.yticks(fontsize=fs)
 plt.grid(True)
+plt.tight_layout()
 plt.savefig('plot2.png')
+
+plt.figure()
+plt.loglog(dt, error_mec, 'r+-', linewidth=lw)
+plt.xlabel(r"\Delta t [s]", fontsize=fs)
+plt.ylabel('Erreur sur l\'énergie mécanie [J]', fontsize=fs)
+plt.xticks(fontsize=fs)
+plt.yticks(fontsize=fs)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('Erreurs_energie.png')
 
 """
 Si on n'a pas la solution analytique: on représente la quantite voulue
@@ -98,6 +126,7 @@ plt.ylabel('v_y [m/s]', fontsize=fs)
 plt.xticks(fontsize=fs)
 plt.yticks(fontsize=fs)
 plt.grid(True)
+plt.tight_layout()
 plt.savefig('plot3.png')
 
 plt.show()
