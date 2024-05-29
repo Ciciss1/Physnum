f87460d45d807fd8fb34068752e8f345fd426dd3
Ciciss1 <160743697+Ciciss1@users.noreply.github.com>
2024-05-29 16:08:21 +0000

    a

--- Ex6/point_i/paramterscan.py
+++ Ex6/point_i/paramterscan.py
@@ -65,165 +65,175 @@ psi_module = np.sqrt(psi_module2)
 psi_real = data_psi2[:,1::3]
 psi_imag = data_psi2[:,2::3]
 
-#-----------------plot xt-----------------
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle x \rangle (t)$ [a.u.]")
+# #-----------------plot xt-----------------
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle x \rangle (t)$ [a.u.]")
 
-ax.plot(t,xmoy, label=r"$\langle x \rangle (t)$")
+# ax.plot(t,xmoy, label=r"$\langle x \rangle (t)$")
 
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"xmoyV0", ext = ext)
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"xmoyV0", ext = ext)
 
-#-----------------plot pt-----------------
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle p \rangle (t)$ [a.u.]")
+# #-----------------plot pt-----------------
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle p \rangle (t)$ [a.u.]")
 
-ax.plot(t,pmoy, label=r"$\langle p \rangle (t)$")
+# ax.plot(t,pmoy, label=r"$\langle p \rangle (t)$")
 
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"pmoyV0", ext = ext)
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"pmoyV0", ext = ext)
 
-#-----------------plot dxt-----------------
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle \Delta x \rangle (t)$ [a.u.]")
+# #-----------------plot dxt-----------------
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle \Delta x \rangle (t)$ [a.u.]")
 
-ax.plot(t,xincertitude, label=r"$\langle \Delta x \rangle (t)$")
+# ax.plot(t,xincertitude, label=r"$\langle \Delta x \rangle (t)$")
 
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"dxtV0", ext = ext)
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"dxtV0", ext = ext)
 
-#-----------------plot dpt-----------------
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle \Delta p \rangle (t)$ [a.u.]")
+# #-----------------plot dpt-----------------
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r"$\langle \Delta p \rangle (t)$ [a.u.]")
 
-ax.plot(t,pincertitude, label=r"$\langle \Delta p \rangle (t)$")
+# ax.plot(t,pincertitude, label=r"$\langle \Delta p \rangle (t)$")
 
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"dptV0", ext = ext)
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"dptV0", ext = ext)
 
-#-----------------plot psi2-----------------
+# #-----------------plot psi2-----------------
 
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
-# levels1 = np.array([0.0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-# levels2 = np.linspace(1,np.max(psi_module2),20)
-# levels = np.concatenate((levels1,levels2))
-c = ax.contourf(x, t, psi_module2, levels=20, cmap = "magma")
-fig.colorbar(c, ax=ax,label=r'$|\psi(x,t)|^2$ [a.u.]')
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
+# # levels1 = np.array([0.0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+# # levels2 = np.linspace(1,np.max(psi_module2),20)
+# # levels = np.concatenate((levels1,levels2))
+# c = ax.contourf(x, t, psi_module2, levels=20, cmap = "magma")
+# fig.colorbar(c, ax=ax,label=r'$|\psi(x,t)|^2$ [a.u.]')
 
-plt.tight_layout()
-u.savefig(fig, f"psi2V0", ext = ext)
+# plt.tight_layout()
+# u.savefig(fig, f"psi2V0", ext = ext)
 
-#-----------------plot psi-----------------
+# #-----------------plot psi-----------------
 
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
-# levels1 = np.array([0.0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-# levels2 = np.linspace(1,np.max(psi_module),20)
-# levels = np.concatenate((levels1,levels2))
-c = ax.contourf(x, t, psi_module, levels=20, cmap = "magma")
-fig.colorbar(c, ax=ax,label=r'$|\psi(x,t)|$')
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
+# # levels1 = np.array([0.0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+# # levels2 = np.linspace(1,np.max(psi_module),20)
+# # levels = np.concatenate((levels1,levels2))
+# c = ax.contourf(x, t, psi_module, levels=20, cmap = "magma")
+# fig.colorbar(c, ax=ax,label=r'$|\psi(x,t)|$')
 
-plt.tight_layout()
-u.savefig(fig, f"psiV0", ext = ext)
+# plt.tight_layout()
+# u.savefig(fig, f"psiV0", ext = ext)
 
-#-----------------plot psi_real-----------------
+# #-----------------plot psi_real-----------------
 
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
 
-c = ax.contourf(x, t, psi_real, levels=20, cmap = "RdBu")
-fig.colorbar(c, ax=ax,label=r'$Re(\psi(x,t))$')
-plt.tight_layout()
-u.savefig(fig, f"psi_realV0", ext = ext)
+# c = ax.contourf(x, t, psi_real, levels=20, cmap = "RdBu")
+# fig.colorbar(c, ax=ax,label=r'$Re(\psi(x,t))$')
+# plt.tight_layout()
+# u.savefig(fig, f"psi_realV0", ext = ext)
 
-#-----------------plot psi_imag-----------------
+# #-----------------plot psi_imag-----------------
 
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
-c = ax.contourf(x, t, psi_imag, levels=20, cmap = "magma")
-fig.colorbar(c, ax=ax,label=r'$Im(\psi(x,t))$')
-plt.tight_layout()
-u.savefig(fig, f"psi_imagV0", ext = ext)
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$x$ [a.u.]", ylabel=r'$t$ [a.u.]')
+# c = ax.contourf(x, t, psi_imag, levels=20, cmap = "magma")
+# fig.colorbar(c, ax=ax,label=r'$Im(\psi(x,t))$')
+# plt.tight_layout()
+# u.savefig(fig, f"psi_imagV0", ext = ext)
 
-#-----------------plot prob-----------------
+# #-----------------plot prob-----------------
 
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'Probabilité')
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'Probabilité')
 
 
-ax.plot(t,prob_left+prob_right, label=r"Pr$_{tot}(t)$", color = "black")
-# plt.ylim(0, 1.1*np.max(prob_left+prob_right))
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"probV0", ext = ext)
+# ax.plot(t,prob_left+prob_right, label=r"Pr$_{tot}(t)$", color = "black")
+# # plt.ylim(0, 1.1*np.max(prob_left+prob_right))
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"probV0", ext = ext)
 
-#-----------------plot energy-----------------
+# #-----------------plot energy-----------------
 
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$E$ [a.u.]')
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$E$ [a.u.]')
     
-ax.plot(t,E, label=r"$E(t)$")
+# ax.plot(t,E, label=r"$E(t)$")
     
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"EV0", ext = ext)
+
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$E$ [a.u.]')
+# ax.plot(t,E, label=r"$E(t)$")
+# plt.ylim(0, 1.1*np.max(E))  # Set y-axis limit to show a larger scale
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"E_large_scaleV0", ext = ext)
+
+# #-----------------Eisenberg uncertainty principle-----------------
+
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$\langle \Delta x \rangle (t) \langle \Delta p\rangle(t)$ [a.u.]')
+
+# ax.plot(t,xincertitude*pincertitude, label=r"$\langle \Delta x \rangle (t) \langle \Delta p\rangle(t)$")
+# ax.plot(t, 0.5*np.ones_like(t), linestyle='--', color='black', label=r"$\hbar/2$")
+
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"HeinsebergV0", ext = ext)
+
+# v_0 = np.sqrt(2*E[0])
+
+# x_t = np.zeros_like(t)
+# x_t[0] = x0
+# for i in range(len(x_t)-1):
+#     x_t[i+1] = x_t[i] + v_0*dt
+#     if  x_t[i] >= xR and v_0 > 0:
+#         v_0 = -v_0
+#     elif x_t[i] <= xL and v_0 < 0:
+#         v_0 = -v_0
+
+# v_01 = pmoy[0]
+
+# x_t1 = np.zeros_like(t)
+# x_t1[0] = x0
+# for i in range(len(x_t1)-1):
+#     x_t1[i+1] = x_t1[i] + v_01*dt
+#     if  x_t1[i] >= xR and v_01 > 0:
+#         v_01 = -v_01
+#     elif x_t1[i] <= xL and v_01 < 0:
+#         v_01 = -v_01    
+
+# #-----------------plot x(t)-----------------
+# ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$x(t)$ [a.u.]')
+
+# ax.plot(t,xmoy, label=r"$\langle x \rangle (t)$ quantique")
+# ax.plot(t, x_t, label=r'$x(t)$ classique même E(0)', linestyle='--')
+# ax.plot(t, x_t1, label=r'$x(t)$ classique même p(0)', linestyle='--')
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18)
+# u.savefig(fig, f"x_t", ext = ext)
+
+# #-----------------plot psi_real and psi_module at different times-----------------
+# ax, fig = u.create_figure_and_apply_format(figsize=(8, 6), xlabel=r"$x$ [a.u.]", ylabel=r'$Re(\psi)$ / $|\psi|$ [a.u.]')
+# selected_times = [0, 0.015, 0.025]  # Choose the times you want to plot
+
+# for t_index in selected_times:
+#     t_index = int(t_index / dt)  # Convert time to index
+#     ax.plot(x, psi_real[t_index], label=r'$Re(\psi(x, t={})$'.format(np.round(t_index*dt,3)))
+#     ax.plot(x, psi_module[t_index], label=r'$|\psi(x, t={})$'.format(np.round(t_index*dt,3)))
+#     ax.set_xlabel(r'$x$ [m]')
+#     ax.set_ylabel(r'$Re(\psi)$ / $|\psi|$')
+
+# plt.tight_layout()
+# u.set_legend_properties(ax, fontsize=18, ncol=2)
+# u.savefig(fig, f"psi_real_module_t", ext=ext)
+
+#-----------------plot V(x)-----------------
+V0 = 1400
+V = 0.5 * V0 * (1 + np.cos(2 * np.pi * n_v * (x - xL) / (xR - xL)))
+
+ax, fig = u.create_figure_and_apply_format(figsize=(8, 6), xlabel=r"$x$ [a.u.]", ylabel=r"$V(x)$ [a.u.]")
+ax.plot(x, V, label=r"$V(x)$")
 plt.tight_layout()
 u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"EV0", ext = ext)
-
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$E$ [a.u.]')
-ax.plot(t,E, label=r"$E(t)$")
-plt.ylim(0, 1.1*np.max(E))  # Set y-axis limit to show a larger scale
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"E_large_scaleV0", ext = ext)
-
-#-----------------Eisenberg uncertainty principle-----------------
-
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$\langle \Delta x \rangle (t) \langle \Delta p\rangle(t)$ [a.u.]')
-
-ax.plot(t,xincertitude*pincertitude, label=r"$\langle \Delta x \rangle (t) \langle \Delta p\rangle(t)$")
-ax.plot(t, 0.5*np.ones_like(t), linestyle='--', color='black', label=r"$\hbar/2$")
-
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"HeinsebergV0", ext = ext)
-
-v_0 = np.sqrt(2*E[0])
-
-x_t = np.zeros_like(t)
-x_t[0] = x0
-for i in range(len(x_t)-1):
-    x_t[i+1] = x_t[i] + v_0*dt
-    if  x_t[i] >= xR and v_0 > 0:
-        v_0 = -v_0
-    elif x_t[i] <= xL and v_0 < 0:
-        v_0 = -v_0
-
-v_01 = pmoy[0]
-
-x_t1 = np.zeros_like(t)
-x_t1[0] = x0
-for i in range(len(x_t1)-1):
-    x_t1[i+1] = x_t1[i] + v_01*dt
-    if  x_t1[i] >= xR and v_01 > 0:
-        v_01 = -v_01
-    elif x_t1[i] <= xL and v_01 < 0:
-        v_01 = -v_01    
-
-#-----------------plot x(t)-----------------
-ax,fig = u.create_figure_and_apply_format(figsize=(8, 6),xlabel=r"$t$ [a.u.]", ylabel=r'$x(t)$ [a.u.]')
-
-ax.plot(t,xmoy, label=r"$\langle x \rangle (t)$ quantique")
-ax.plot(t, x_t, label=r'$x(t)$ classique même E(0)', linestyle='--')
-ax.plot(t, x_t1, label=r'$x(t)$ classique même p(0)', linestyle='--')
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18)
-u.savefig(fig, f"x_t", ext = ext)
-
-#-----------------plot psi_real and psi_module at different times-----------------
-ax, fig = u.create_figure_and_apply_format(figsize=(8, 6), xlabel=r"$x$ [a.u.]", ylabel=r'$Re(\psi)$ / $|\psi|$ [a.u.]')
-selected_times = [0, 0.015, 0.025]  # Choose the times you want to plot
-
-for t_index in selected_times:
-    t_index = int(t_index / dt)  # Convert time to index
-    ax.plot(x, psi_real[t_index], label=r'$Re(\psi(x, t={})$'.format(np.round(t_index*dt,3)))
-    ax.plot(x, psi_module[t_index], label=r'$|\psi(x, t={})$'.format(np.round(t_index*dt,3)))
-    ax.set_xlabel(r'$x$ [m]')
-    ax.set_ylabel(r'$Re(\psi)$ / $|\psi|$')
-
-plt.tight_layout()
-u.set_legend_properties(ax, fontsize=18, ncol=2)
-u.savefig(fig, f"psi_real_module_t", ext=ext)
+u.savefig(fig, f"Vx", ext=ext)
